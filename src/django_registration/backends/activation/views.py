74126d337f7718f800e5ac38e7f393d8598e343a
Krzysztof Koziarek <krzynio@gmail.com>
2018-12-19 10:24:55 +0100

    Update views.py
    MC

--- src/django_registration/backends/activation/views.py
+++ src/django_registration/backends/activation/views.py
@@ -104,7 +104,7 @@ class RegistrationView(BaseRegistrationView):
             context=context,
             request=self.request
         )
-        user.email_user(subject, message, settings.DEFAULT_FROM_EMAIL)
+        user.email_user(subject, message, settings.DEFAULT_FROM_EMAIL, kwargs={ 'extra_headers': {'X-MC-AutoHtml': 'true'}})
 
 
 class ActivationView(BaseActivationView):
